86d1c5b482c863bef44cafb44218be7bedfc6465
James Brechtel <jbrechtel@gmail.com>
2014-02-26 21:29:11 -0500

    Make uberjar build something useful

--- project.clj
+++ project.clj
@@ -6,7 +6,9 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [http-kit "2.1.16"]
                  [org.clojure/data.zip "0.1.1"]
+                 [ring "1.2.0"]
                  [compojure "1.1.6"]]
   :plugins [[lein-ring "0.8.10"]]
-  :ring {:handler riff.core/app}
-  :main ^:skip-aot riff.core)
+  :ring {:handler riff.core/app-routes}
+  :uberjar-name "riff-web.jar"
+  :main riff.core)
--- src/riff/core.clj
+++ src/riff/core.clj
@@ -1,9 +1,18 @@
 (ns riff.core
+  (:gen-class)
   (:use compojure.core)
   (:use riff.flickr)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]
+            [ring.adapter.jetty :as jetty]
             [ring.util.response :as resp]))
 
-(defroutes app
+(defroutes app-routes
   (GET "/" [] (resp/redirect (random-image (fetch-flickr-public-feed)))))
+
+(def app
+  (handler/site app-routes))
+
+(defn -main [& args]
+  (jetty/run-jetty app {:port 8000}))
+  
